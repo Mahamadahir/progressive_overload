61a40c9190d2f42964a6b0b49f1357fdbe106272
Mahamadahir <mahamad@users.noreply.github.com>
2026-07-14 19:40:07 +0100

    fix: resolve release keystore path

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -42,7 +42,7 @@ android {
             if (keystorePropertiesFile.exists()) {
                 keyAlias = keystoreProperties["keyAlias"] as String
                 keyPassword = keystoreProperties["keyPassword"] as String
-                storeFile = file(keystoreProperties["storeFile"] as String)
+                storeFile = rootProject.file(keystoreProperties["storeFile"] as String)
                 storePassword = keystoreProperties["storePassword"] as String
             }
         }
